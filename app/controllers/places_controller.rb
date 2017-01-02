cfd78895aa27bd97f77d60c4669f6670b5ea040d
jonchrisong <jonchrisong@gmail.com>
2017-01-02 11:05:07 +0000

    Handling form submission

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -2,14 +2,22 @@ class PlacesController < ApplicationController
   def index
 
     @places = Place.all.order("created_at desc").paginate(page: params[:page], per_page: 3)
-end
+  end
 
   def new
     @place = Place.new
   end
 
+  def create
+    Place.create(place_params)
+    redirect_to root_path
+  end
 
+Private
 
+def place_params
+  params.required(:place).permit(:name, :description, :address)
+end
 
 
 
