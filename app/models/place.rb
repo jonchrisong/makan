8782272e745290c009f1315abff85516c3d09437
jonchrisong <jonchrisong@gmail.com>
2017-01-25 15:35:03 +0000

    more validations

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,7 +1,9 @@
 class Place < ActiveRecord::Base
 
   belongs_to :user
-  validates :name, presence: true
+  validates :name, presence: { message: "Name is a required field"}, length: {minimum: 4, message: "Name has to be at least 3 characters"}
+  validates :address, presence: { message: "Address is a required field"}
+  validates :description, presence: { message: "Description is a required field"}
 
 
 end
