c8ee498078f42614d92cdaa418f9baa395e6f083
jonchrisong <jonchrisong@gmail.com>
2017-01-28 02:55:49 +0000

    Adding comments to the DB

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,6 +1,8 @@
 class Place < ActiveRecord::Base
 
   belongs_to :user
+  has_many :comments
+
   geocoded_by :address
   after_validation :geocode
   validates :name, presence: { message: "Name is a required field"}, length: {minimum: 4, message: "Name has to be at least 3 characters"}
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,7 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
          has_many :places
-end
+         has_many :comments
+
+       end
+       
